d4e4e086c12cd7aa908bf71fc82dc5510af73a27
JainyMartins <jainy.martins@mercadolivre.com>
2023-06-20 09:23:05 -0300

    Refatoracao Iterativa

--- fibonacci/functionFibonacci.go
+++ fibonacci/functionFibonacci.go
@@ -2,9 +2,17 @@ package fibonacci
 
 
 func Fibonacci(n int) int {
-	if (n < 2) {
-		return n;
-	} else {
-		return Fibonacci(n - 1) + Fibonacci(n - 2);
+	f := 0
+	ant := 0
+
+	for i := 1; i <= n; i++ {
+		if i == 1 {
+			f = 1
+			ant = 0
+		} else {
+			f += ant
+			ant = f - ant
+		}
 	}
+	return f
 }
